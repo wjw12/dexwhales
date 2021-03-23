fd5c0a50c129580999d313e55c839073e902135a
wjw12 <jiewenw96@gmail.com>
2021-03-23 16:08:34 -0400

    gitcoin grant

--- src/Tip.js
+++ src/Tip.js
@@ -1,27 +1,6 @@
 import './App.css';
 
 export default function Tip() {
-    function makeTip(value) {
-        var web3 = window.web3
-        if (typeof web3 === 'undefined') {
-            return alert('You need to install MetaMask to use this feature.  https://metamask.io')
-        }
-
-        var user_address = web3.eth.accounts[0]
-
-        if (!user_address) {
-            return alert('Please connect with your wallet')
-        }
-        web3.eth.sendTransaction({
-            to: '0x108616595d907Fe2e6E60DAC7004f96E14dA69Ba',
-            from: user_address,
-            value: web3.toWei(value, 'ether')
-        }, function (err, transactionHash) {
-        if (err) return alert('Oops: ' + err.message)
-        alert('Thank you!')
-        })    
-    }
-
 
     return (
         <div style={{
@@ -33,19 +12,7 @@ export default function Tip() {
             textAlign: 'right'
         }}>
             <p className="compact-paragraph">
-            🌞 Tip me:
-            </p>
-            <p className="compact-paragraph">
-                <a href="/#" onClick={() => makeTip(0.001)}>Ξ0.001</a>
-            </p>
-            <p className="compact-paragraph">
-                <a href="/#" onClick={() => makeTip(0.01)}>Ξ0.010</a>
-            </p>
-            <p className="compact-paragraph">
-                <a href="/#" onClick={() => makeTip(0.1)}>Ξ0.100</a>
-            </p>
-            <p className="compact-paragraph">
-                <a href="/#" onClick={() => makeTip(1)}>Ξ1.000</a>
+            🌞 Support my <a href="https://gitcoin.co/grants/2393/dex-whales">Gitcoin Grant</a>
             </p>
             <p className="compact-paragraph">
                 Developer: <a href="https://twitter.com/artoriamaster">@artoriamaster</a>
